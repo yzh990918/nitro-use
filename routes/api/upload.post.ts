1ed4315a342cd71b0cfa0b53e29b17bc7f035d1e
yzh990918 <251205668@qq.com>
2023-05-19 16:03:31 +0800

    chore: update

--- routes/api/upload.post.ts
+++ routes/api/upload.post.ts
@@ -1,15 +1,10 @@
-import fse from 'fs-extra'
 import dayjs from 'dayjs';
-import { createHash } from 'crypto'
+import {createHash} from 'crypto'
 
 export default eventHandler(async (event) => {
   const form = await readMultipartFormData(event)
   const AssetsBaseURL = 'http://' + event.req.headers.host + '/file'
   const res = []
-
-  const fileDir = `data/${dayjs().format('YYYY-MM-DD')}`
-  if (!fse.existsSync(fileDir))
-    await fse.mkdir(fileDir)
 
   form.forEach(async item => {
     const extendName = item.filename.split('.').pop()
@@ -18,7 +13,8 @@ export default eventHandler(async (event) => {
       filename: item.filename,
       url: `${AssetsBaseURL}/${dayjs().format('YYYY-MM-DD')}/${fileName}.${extendName}`
     })
-    await fse.writeFile(`${fileDir}/${fileName}.${extendName}`, item.data)
+
+    await useStorage().setItemRaw(`file/${dayjs().format('YYYY-MM-DD')}/${fileName}.${extendName}`,item.data)
   })
 
   return {
--- routes/file/[...name].get.ts
+++ routes/file/[...name].get.ts
@@ -1,8 +1,9 @@
-import fs from "fs";
 import path from 'path'
+import {Readable} from 'stream'
 
 export default defineEventHandler(async (event) => {
-  const base = "data";
-  const filePath = path.join(base, event.context.params.name);
-  return sendStream(event, fs.createReadStream(filePath));
+  const filePath = path.join('file',event.context.params.name);
+
+  const buffer = await useStorage().getItemRaw(filePath) as Buffer
+  return sendStream(event, Readable.from(buffer));
 });
