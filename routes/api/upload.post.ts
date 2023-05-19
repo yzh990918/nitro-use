5ee3d0297d81f7ddea906c8039fa56149c3c11f0
yzh990918 <251205668@qq.com>
2023-05-19 15:47:35 +0800

    chore: update

--- routes/api/upload.post.ts
+++ routes/api/upload.post.ts
@@ -1,12 +1,16 @@
+import fse from 'fs-extra'
 import dayjs from 'dayjs';
-import {createHash} from 'crypto'
+import { createHash } from 'crypto'
 
 export default eventHandler(async (event) => {
   const form = await readMultipartFormData(event)
   const AssetsBaseURL = 'http://' + event.req.headers.host + '/file'
   const res = []
 
-  
+  const fileDir = `data/${dayjs().format('YYYY-MM-DD')}`
+  if (!fse.existsSync(fileDir))
+    await fse.mkdir(fileDir)
+
   form.forEach(async item => {
     const extendName = item.filename.split('.').pop()
     const fileName = createHash('md5').update(item.data).digest('hex')
@@ -14,8 +18,7 @@ export default eventHandler(async (event) => {
       filename: item.filename,
       url: `${AssetsBaseURL}/${dayjs().format('YYYY-MM-DD')}/${fileName}.${extendName}`
     })
-
-    await useStorage().setItemRaw(`file/${dayjs().format('YYYY-MM-DD')}/${fileName}.${extendName}`,item.data)
+    await fse.writeFile(`${fileDir}/${fileName}.${extendName}`, item.data)
   })
 
   return {
